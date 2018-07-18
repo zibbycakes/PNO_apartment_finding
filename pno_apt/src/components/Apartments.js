4685c58a390a442cdb1dd8b023f357b03ae2cc2e
Allyson Walpole <zibbycakes@gmail.com>
2018-07-18 14:56:27 -0500

    i think apartments is formatted enough. Added a lot of data for json. Uses maps now. Scalable.

--- pno_apt/src/components/Apartments.js
+++ pno_apt/src/components/Apartments.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import apt_info from '../apt_info.json';
-import {Container, Row, Col, Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle} from 'reactstrap';
+import {Container, Row, Col, Button, Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle} from 'reactstrap';
 
 class Apartments extends Component
 {
@@ -10,27 +10,57 @@ class Apartments extends Component
         this.apartments = apt_info.apartments;
         this.row_limit = 2;
         this.state = {
-            formattedArray : []
+            formattedArray : [],
+            formattedMapURL: ""
         }
     }
 
     componentDidMount() 
     {
+        document.title = "Apartments";
         this.formatDisplayFromJSON();
+        this.formatMapSourceURL();
+    }
+
+    formatMapSourceURL() 
+    {
+        var tempURL = "https://maps.googleapis.com/maps/api/staticmap?center=8058%20Dominion%20Pkwy,%20Plano,%20TX%2075024&zoom=13&size=800x400&maptype=roadmap&key=AIzaSyByNJOSX4S3ez2NM_g9na2hstoUCCP-0a4&markers=color:red%7Clabel:C%7C8058%20Dominion%20Pkwy,%20Plano,%20TX%2075024";
+        var tempseg = "";
+        for(let apt in this.apartments)
+        {
+            var a = this.apartments[apt];
+            //something something location. example:
+            //&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318
+            //&markers=color:red%7Clabel:C%7C40.718217,-73.998284
+            tempseg="&markers=color:"+a.mapcolor+"%7Clabel:"+a.maplabel+"%7C"+a.location;
+            tempURL+=tempseg;
+        }
+        this.setState({formattedMapURL:tempURL});
     }
 
     formatDisplayFromJSON() 
     {
+        var colorLibrary =
+        {
+            purple: "#8c71a3",
+            orange: "#d08931",
+            blue: "#86acf6",
+            brown: "#917750"
+        }
         var tempColArray =  [];
         console.log(this.apartments);
         for(let a in this.apartments) {
             tempColArray.push(
                 <Col key ={this.apartments[a].name}>
                     <Card>
-                        <CardImg href={"apartment/"+this.apartments[a].id} top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
-                        <CardTitle><a href={"/apartment/" + this.apartments[a].id}>{this.apartments[a].name}</a></CardTitle>
+                        <CardBody style={{height:'20px',background:colorLibrary[this.apartments[a].mapcolor]}}>
+                            <br />
+                        </CardBody>
+                        <CardImg top height="337px" width="100%" src={this.apartments[a].gridImageLoc} alt="Card image cap" />
+                        <CardTitle><a href={"/apartment/" + this.apartments[a].id}>{this.apartments[a].name}</a> <Button style={{float:'right'}} outline color='danger' size="sm" target="_blank" href={"https://www.google.com/maps/place/"+this.apartments[a].location}><img width="24" height="24" src="https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678111-map-marker-512.png" /></Button></CardTitle>
                         <CardSubtitle>{this.apartments[a].location}</CardSubtitle>
-                        <CardText>{this.apartments[a].rent}</CardText>
+                        <br />
+                        <CardText>{this.apartments[a].lowestRent}</CardText>
                     </Card>
                 </Col>
             );
@@ -60,9 +90,14 @@ class Apartments extends Component
         );*/
         return (
             <div>
+                <h1 style={{textAlign:'center'}}>Apartments</h1>
+                <br />
                 <Container>
                     {this.state.formattedArray}
                 </Container>
+                <p align="center">
+                    <img src={this.state.formattedMapURL} /> 
+                </p>  
             </div>
         );
     }
