efd5e767ba68adaf8ce2254009c97fd24b7b3b68
Allyson Walpole <zibbycakes@gmail.com>
2018-07-18 13:14:15 -0500

    added and now read from JSON on apartments page. May need to reformat.

--- pno_apt/src/components/Apartments.js
+++ pno_apt/src/components/Apartments.js
@@ -1,13 +1,68 @@
 import React, {Component} from 'react';
+import apt_info from '../apt_info.json';
+import {Container, Row, Col, Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle} from 'reactstrap';
 
 class Apartments extends Component
 {
+    constructor() 
+    {
+        super();
+        this.apartments = apt_info.apartments;
+        this.row_limit = 2;
+        this.state = {
+            formattedArray : []
+        }
+    }
+
+    componentDidMount() 
+    {
+        this.formatDisplayFromJSON();
+    }
+
+    formatDisplayFromJSON() 
+    {
+        var tempColArray =  [];
+        console.log(this.apartments);
+        for(let a in this.apartments) {
+            tempColArray.push(
+                <Col key ={this.apartments[a].name}>
+                    <Card>
+                        <CardImg href={"apartment/"+this.apartments[a].id} top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+                        <CardTitle><a href={"/apartment/" + this.apartments[a].id}>{this.apartments[a].name}</a></CardTitle>
+                        <CardSubtitle>{this.apartments[a].location}</CardSubtitle>
+                        <CardText>{this.apartments[a].rent}</CardText>
+                    </Card>
+                </Col>
+            );
+        }
+        var tempRowArray = [];
+        var countCol = 0;
+        var numSplices = Math.ceil(tempColArray.length / this.row_limit);
+        for(var i = 0; i < numSplices; i++)
+        {
+                tempRowArray.push(
+                    <Row key={countCol++}>
+                        {tempColArray.splice(0,this.row_limit)}
+                    </Row> 
+                );
+                tempRowArray.push(<br />)
+        }
+        this.setState({formattedArray: tempRowArray});
+    }
     render()
     {
-        return (
+        console.log(apt_info.apartments);
+        /*return (
             <div>
                 <p>Welcome to the apartments page</p>
                 <a href="/apartment/1">Apartment 1</a>
+            </div>
+        );*/
+        return (
+            <div>
+                <Container>
+                    {this.state.formattedArray}
+                </Container>
             </div>
         );
     }
