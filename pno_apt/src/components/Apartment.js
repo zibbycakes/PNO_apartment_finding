59b8c8dbf1106ad5a78c6c40cfcf444432a36224
Allyson Walpole <zibbycakes@gmail.com>
2018-07-19 19:33:48 -0500

    added basic layout for individual apartments. Need floorplan info in json

--- pno_apt/src/components/Apartment.js
+++ pno_apt/src/components/Apartment.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import {Button, ListGroup, ListGroupItem, Container, Row, Col, Table} from 'reactstrap';
+import apt_info from '../apt_info.json';
 
 class Apartment extends Component
 {
@@ -7,6 +9,8 @@ class Apartment extends Component
         super();
         this.id = props.match.params.number;
         console.log(this.id);
+        console.log(apt_info.apartments[this.id]);
+        this.info = apt_info.apartments[this.id];
     }
     
     componentDidMount()
@@ -18,7 +22,49 @@ class Apartment extends Component
     {
         return (
             <div>
-                This is Apartment {this.id}
+                <Button style={{marginTop:"50px", marginBottom:"50px"}} color="primary" href="/apartments">← Back to Apartments</Button>
+                <h1 style={{textAlign:'center'}}>{this.info.name}</h1>
+                <br />
+                <Container style={{maxWidth:"1500px"}}>
+                    <Row>
+                        <Col>
+                            <img src={this.info.gridImageLoc} height="478px" width="100%"/>
+                        </Col>
+                        <Col style={{paddingTop:'135px'}}>
+                            <br />
+                            <h3 style={{textAlign:'center'}}>Contact Info</h3>
+                            <ListGroup>
+                                <ListGroupItem><strong>Address:</strong> <a target="_blank" href={"https://www.google.com/maps/place/"+this.info.location}>{this.info.location}</a></ListGroupItem>
+                                <ListGroupItem><strong>Phone #:</strong> {this.info.contactNumber}</ListGroupItem>
+                                <ListGroupItem><strong>Website:</strong> <a href={this.info.website}>{this.info.website}</a></ListGroupItem>
+                                <ListGroupItem><strong>Appointment Scheduled?</strong> No</ListGroupItem>
+                            </ListGroup>
+                        </Col>
+                    </Row>
+                    <br />
+                    <Row>
+                        <h3 style={{textAlign:'center'}}>Floorplans</h3>
+                        <br />
+                        <Table hover>
+                            <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Image</th>
+                                <th>Rent</th>
+                                <th>Availibility</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>testi</td>
+                                <td>testr</td>
+                                <td>testa</td>
+                            </tr>
+                            </tbody>
+                        </Table>
+                    </Row>
+                </Container>
             </div>
         );
     }
